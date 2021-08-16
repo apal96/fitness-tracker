11ade6a91ce5af926898432db05a2fff1b014d54
apal <alishapal96@gmail.com>
2021-08-16 09:06:49 -0700

    add model

--- models/Workout.js
+++ models/Workout.js
@@ -2,12 +2,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  type:String,
-  name:String,
-  duration:Ineteger,
-  weight:Ineteger,
-  reps:Ineteger,
-  sets:Ineteger
+  type:{
+    type: String,
+    trim: true,
+    required: "Enter a type of workout"
+  },
+  name:{
+    type: String,
+    trim: true,
+    required: "Enter a name for the workout"
+  },
+  duration:{
+    type: Integer
+  },
+  weight:{
+    type: Double
+  },
+  reps:{
+    type: Integer
+  },
+  sets:{
+    type: Integer
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 
 });
 
